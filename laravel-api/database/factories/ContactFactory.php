84e6c9aef52d9bf4e66b76804b1ceea93614d570
Daniel <dregojevic@gmail.com>
2017-09-04 20:36:11 +0200

    First commit

---
+++ laravel-api/database/factories/ContactFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+use Faker\Generator as Faker;
+
+$factory->define(\App\Models\Contact::class, function(Faker $faker){
+  return [
+    'first_name' => $faker->firstName,
+    'last_name' => $faker->lastName,
+    'email' => $faker->unique()->safeEmail
+  ];
+});
